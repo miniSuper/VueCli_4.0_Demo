52e855e773cceae1a7964d6e7ac05485e056612b
WuJiaXiang <516143986@qq.com>
2020-04-16 21:19:09 +0800

    添加webpack的一些配置

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img
+      alt="Vue logo"
+      src="./assets/logo.png"
+    >
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -12,6 +15,19 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  mounted() {
+    this.test()
+  },
+  methods: {
+    test() {
+      console.log(window)
+      console.log('VueRouter', window.VueRouter)
+      console.log('Axios', window.Axios)
+      console.log('MintUI', window.MINT)
+      console.log('Moment', window.Moment)
+      console.log('Vuex', window.Vuex)
+    }
   }
 }
 </script>
